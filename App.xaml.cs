166a1118e81a4a4e24a7dc34ff05a61df2840342
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2024-08-01 14:50:57 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIAppTempoAgora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
